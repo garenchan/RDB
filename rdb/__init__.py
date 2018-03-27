99c833b8ee1839ab7b59703cc94beef597121472
chengkang <1412950785@qq.com>
2018-03-27 09:10:37 +0800

    小调整

--- rdb/__init__.py
+++ rdb/__init__.py
@@ -1,7 +1,8 @@
 from __future__ import absolute_import
 
+from rdb.rdb import Rdb, debugger, set_trace
+
 __all__ = [
     'Rdb', 'debugger', 'set_trace'
 ]
 
-from rdb.rdb import Rdb, debugger, set_trace
